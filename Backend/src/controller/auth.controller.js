d65dcee91bad128cd3de5cac7ebe8a71d3a13fec
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-04 23:19:13 +0530

    Basic Authentication completed

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -153,4 +153,17 @@ export const logout = async (req, res) => {
   }
 };
 
-export const check = async (req, res) => {};
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User Aunthenticated Successfully.",
+      user: req.user,
+    });
+  } catch (error) {
+    console.log("Error checking user", error);
+    res.status(500).json({
+      error: "Error checking user",
+    });
+  }
+};
---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+import { db } from "../Libs/db.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized - No token found.",
+      });
+    }
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res.status(401).json({
+        message: "Unauthorized - Invalid token.",
+      });
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        image: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found.",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error Authentication user : ", error);
+    res.status(500).json({ message: "Error authenticating user." });
+  }
+};

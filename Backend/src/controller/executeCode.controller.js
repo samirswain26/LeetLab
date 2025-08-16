7ff158ce3c40ae4b4df5437a85ec12cc7def997b
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-16 20:22:45 +0530

    added execution endpoints

--- Backend/src/controller/executeCode.controller.js
+++ Backend/src/controller/executeCode.controller.js
@@ -4,6 +4,7 @@ export const executeCode = async (req, res) => {
   try {
     const { source_code, language_id, stdin, expected_outputs, problemId } =
       req.body;
+
     const userId = req.user.id;
 
     // Validate test cases are in the proper array format or not

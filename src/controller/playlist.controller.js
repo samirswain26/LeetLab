2f9c21062c7c511b9503a1acd7e6de483529ef11
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-19 21:38:08 +0530

    Play-list created successfully

---
+++ src/controller/playlist.controller.js
@@ -0,0 +1,42 @@
+import { db } from "../Libs/db.js";
+
+export const createPlaylist = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const userId = req.user.id;
+
+    // if (!name || !description) {
+    //   return res.status(403).json({ error: "All fields are required" });
+    // }
+    console.log(name);
+
+    const playlist = await db.playlist.create({
+      data: {
+        name,
+        description,
+        userId,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "PlayList created successfully",
+      playlist,
+    });
+  } catch (error) {
+    console.log("Error in creating playlist :", error);
+    res.status(500).json({
+      error: "Error in creating playlist",
+    });
+  }
+};
+
+export const getAllListDetails = async (req, res) => {};
+
+export const getPlayListDetails = async (req, res) => {};
+
+export const addProblemstoPlaylist = async (req, res) => {};
+
+export const deletePlaylist = async (req, res) => {};
+
+export const removeProblemFromPlaylist = async (req, res) => {};

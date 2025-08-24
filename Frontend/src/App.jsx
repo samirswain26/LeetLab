5214c84fe8579c1e0af347fd198a37fa0e2e69d7
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-24 12:34:39 +0530

    Signup page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  let auth = null;
+
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route
+          path="/"
+          element={auth ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path="/login"
+          element={!auth ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path="/signup"
+          element={!auth ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;

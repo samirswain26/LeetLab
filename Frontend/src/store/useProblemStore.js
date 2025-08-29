3fb672505916d0fd0dbf55314fdfdbfc4ed7861b
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-29 10:59:12 +0530

    created the Problem Store for validating the data & fetch backend

---
+++ Frontend/src/store/useProblemStore.js
@@ -0,0 +1,52 @@
+import { Create } from "zustand";
+import { axiosInstance } from "../libs/axios";
+import { toast } from "react-hot-toast";
+
+export const useProblemStore = Create((set) => ({
+  problems: [],
+  problem: null,
+  solvedProblems: [],
+  isProblemsLoading: false,
+  isProblemLoading: false,
+
+  getAllProblems: async () => {
+    try {
+      set({ isProblemsLoading: true });
+
+      const res = await axiosInstance.get("/problems/get-all-problems");
+
+      set({ problems: res.data.problems });
+    } catch (error) {
+      console.log("Error getting all problems", error);
+      toast.error(res.data.message || "Error in getting problems");
+    } finally {
+      set({ isProblemsLoading: false });
+    }
+  },
+
+  getProblemById: async (id) => {
+    try {
+      set({ isProblemLoading: true });
+
+      const res = await axiosInstance.get(`/problems/get-problem/${id}`);
+
+      set({ problem: res.data.problem });
+      toast.message(res.data.message);
+    } catch (error) {
+      console.log("Error in getting the problem :", error);
+      toast(res.data.message || "Error in getting the problems");
+    } finally {
+      set({ isProblemLoading: false });
+    }
+  },
+
+  getSolvedProblemByUser: async () => {
+    try {
+      const res = await axiosInstance.get("/problems/get-solved-problems");
+      set({ solvedProblems: res.data.problems });
+    } catch (error) {
+      console.log("Error in getting solved problem by the user:", error);
+      toast(res.data.message || "Error in getting solved problem by the user");
+    }
+  },
+}));

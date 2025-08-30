4003a8ca4bbd260072117d1d28c3e3358cf59705
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-08-30 21:01:25 +0530

    Retrive the data according to the tags and checkList

--- Frontend/src/components/ProblemTable.jsx
+++ Frontend/src/components/ProblemTable.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { use, useMemo, useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Link } from "react-router-dom";
 import { Bookmark, PencilIcon, Trash, TrashIcon, Plus } from "lucide-react";
@@ -8,10 +8,11 @@ const ProblemTable = ({ problems }) => {
 
   const [search, setSearch] = useState("");
   const [difficulty, setDifficulty] = useState("ALL");
-  const [selectedTag, setSeletecTag] = useState("ALL");
+  const [selectedTag, setSelectedTag] = useState("ALL");
   const [currentPage, setCurrentPage] = useState(1);
 
   const difficulties = ["EASY", "MEDIUM", "HARD"];
+
   const allTags = useMemo(() => {
     if (!Array.isArray(problems)) return [];
 
@@ -20,6 +21,35 @@ const ProblemTable = ({ problems }) => {
     problems.forEach((p) => p.tags?.forEach((h) => tagset.add(h)));
     return Array.from(tagset);
   }, [problems]);
+
+  //Filetr the data according to the data comming from the backend to show in the ui when user selects the filter the questions
+  const filteredProblems = useMemo(() => {
+    return (problems || [])
+      .filter((problem) =>
+        problem.title.toLowerCase().includes(search.toLowerCase())
+      )
+      .filter((problem) =>
+        difficulty === "ALL" ? true : problem.difficulty === difficulty
+      )
+      .filter((problem) =>
+        selectedTag === "ALL" ? true : problem.tags?.includes(selectedTag)
+      );
+  }, [problems, search, difficulties, selectedTag]);
+
+  // Pagination for the table data
+  const itemsPerPage = 5;
+  const totalPages = Math.ceil(filteredProblems.length / itemsPerPage);
+  const paginatedProblems = useMemo(() => {
+    return filteredProblems.slice(
+      (currentPage - 1) * itemsPerPage, // (1-1) * 5 => {((0 * 5 ) = 0), ((1 * 5) = 0} (Starting index = 0)
+      currentPage * itemsPerPage // 1 * 5 = {(0, 5), (5, 10) }but slice takes {(0,4),(5,9)} (Ending index = 5)
+    );
+  }, [filteredProblems, currentPage]);
+
+
+  const handleDelete = (id) => {}
+
+  const handleAddToPlaylist = (id) => {}
 
   return (
     <div className="w-full max-w-6xl mx-auto mt-10">
@@ -55,7 +85,7 @@ const ProblemTable = ({ problems }) => {
         <select
           className="select select-bordered bg-base-200"
           value={selectedTag}
-          onChange={(e) => setSeletecTag(e.target.value)}
+          onChange={(e) => setSelectedTag(e.target.value)}
           placeholder="Conditionals"
         >
           <option value="All">All Tags</option>
@@ -79,7 +109,90 @@ const ProblemTable = ({ problems }) => {
             </tr>
           </thead>
           <tbody>
-            
+            {paginatedProblems.length > 0 ? (
+              paginatedProblems.map((problem) => {
+                const isSolved = problem.solvedBy.some(
+                  (user) => user.userId == authUser?.id
+                );
+                return (
+                  <tr key={problem.id}>
+                    <td>
+                      <input
+                        type="checkbox"
+                        checked={isSolved}
+                        readOnly
+                        className="checkbox checkbox-sm"
+                      />
+                    </td>
+                    <td>
+                      <Link
+                        to={`/problem/${problem.id}`}
+                        className="font-semibold hover:underline"
+                      >
+                        {problem.title}
+                      </Link>
+                    </td>
+                    <td>
+                      <div className="flex flex-wrap gap-1">
+                        {(problem.tags || []).map((tag, index) => (
+                          <span
+                            key={index}
+                            className="badge badge-outline badge-warning text-xs font-bold"
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    </td>
+                    <td>
+                      <span
+                        className={`badge font-semibold text-xs text-white ${
+                          problem.difficulty === "EASY"
+                            ? "badge-success"
+                            : problem.difficulty === "MEDIUM"
+                            ? "badge-warning"
+                            : "badge-error"
+                        }`}
+                      >
+                        {problem.difficulty}
+                      </span>
+                    </td>
+                    <td>
+                      <div className="flex flex-col md:flex-row gap-2 items-start md:items-center2">
+                        {authUser?.role === "ADMIN" && (
+                          <div className="flex gap-2">
+                            <button
+                              onClick={() => handleDelete(problem.id)}
+                              className="btn btn-sm btn-error"
+                            >
+                              <TrashIcon className="w-4 h-4 text-white" />
+                            </button>
+                            <button disabled className="btn btn-sm btn-warning">
+                              <PencilIcon className="w-4 h-4 text-white" />
+                            </button>
+                          </div>
+                        )}
+                        <button
+                          className="btn btn-sm btn-outline flex gap-2 items-center"
+                          onClick={() => handleAddToPlaylist(problem.id)}
+                        >
+                          <Bookmark className="w-4 h-4" />
+                          <span className="hidden sm:inline">
+                            Save to Playlist
+                          </span>
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={5} className="text-center py-6 text-gray-500">
+                  No Problems found.
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>

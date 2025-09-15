c66dbe098b4af2dc135d492f28ed7563d434f711
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-09-15 14:14:01 +0530

    Submissions fetched for the particular problem for subscription users

--- Backend/src/controller/subscriptionSubmission.controller.js
+++ Backend/src/controller/subscriptionSubmission.controller.js
@@ -1,32 +1,56 @@
 import { db } from "../Libs/db.js";
 
-export const getAllSubmissions = async(req, res) => {
-    try {
-        const userId = req.user.id
-        const submission = await db.SubscriptionSubmission.findMany({
-            where:{
-                userId: userId
-            }
-        })
+export const getAllSubmissions = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const submission = await db.SubscriptionSubmission.findMany({
+      where: {
+        userId: userId,
+      },
+    });
 
-        if(!submission){
-            return res.status(404).json({
-                error: "Submissions not found"
-            })
-        }
-        res.status(201).json({
-            success: true,
-            message: "Submission fetched successfully",
-            submission
-        })
-    } catch (error) {
-        console.log("submission error is : ", error)
-        res.status(500).json({
-            error: "Failed to fetch submission"
-        })
+    if (!submission) {
+      return res.status(404).json({
+        error: "Submissions not found",
+      });
     }
-}
+    res.status(201).json({
+      success: true,
+      message: "Submission fetched successfully",
+      submission,
+    });
+  } catch (error) {
+    console.log("submission error is : ", error);
+    res.status(500).json({
+      error: "Failed to fetch submission",
+    });
+  }
+};
 
-export const getSubmissionsForProblem = async(req, res) => {}
+export const getSubmissionsForProblem = async (req, res) => {
+  const { problemId } = req.params.problemId;
+  const { userId } = req.user.id;
+  try {
+    const submissions = await db.SubscriptionSubmission.findMany({
+      where: {
+        userId: userId,
+        problemId: problemId,
+      },
+    });
 
-export const getAllTheSubmissionsForProblem = async(req, res) => {}
+    console.log("Get submissions for problem :", submissions);
+
+    res.status(200).json({
+      success: true,
+      message: "submission fetched successfully",
+      submissions,
+    });
+  } catch (error) {
+    console.log(`Submission error is :`.error);
+    res.status(500).json({
+      error: "Failed to fetch submissions.",
+    });
+  }
+};
+
+export const getAllTheSubmissionsForProblem = async (req, res) => {};

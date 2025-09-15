99bb3d31090a338ab67456babe748cd214c81000
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-09-15 13:41:34 +0530

    Subcription submission routes and controller structure added

---
+++ Backend/src/controller/subscriptionSubmission.controller.js
@@ -0,0 +1,7 @@
+import { db } from "../Libs/db.js";
+
+export const getAllSubmissions = async(req, res) => {}
+
+export const getSubmissionsForProblem = async(req, res) => {}
+
+export const getAllTheSubmissionsForProblem = async(req, res) => {}

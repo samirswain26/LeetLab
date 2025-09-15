76d8cf2e7c5a385f26766c19b20df4fd1b9051ca
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-09-15 14:06:32 +0530

    Fetched all submissions

--- Backend/src/controller/subscriptionSubmission.controller.js
+++ Backend/src/controller/subscriptionSubmission.controller.js
@@ -1,6 +1,31 @@
 import { db } from "../Libs/db.js";
 
-export const getAllSubmissions = async(req, res) => {}
+export const getAllSubmissions = async(req, res) => {
+    try {
+        const userId = req.user.id
+        const submission = await db.SubscriptionSubmission.findMany({
+            where:{
+                userId: userId
+            }
+        })
+
+        if(!submission){
+            return res.status(404).json({
+                error: "Submissions not found"
+            })
+        }
+        res.status(201).json({
+            success: true,
+            message: "Submission fetched successfully",
+            submission
+        })
+    } catch (error) {
+        console.log("submission error is : ", error)
+        res.status(500).json({
+            error: "Failed to fetch submission"
+        })
+    }
+}
 
 export const getSubmissionsForProblem = async(req, res) => {}
 

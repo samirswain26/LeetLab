7cec9e4f4e2b701e76744b9c05b553711153fca1
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-09-17 23:46:36 +0530

    create order controller added

--- Backend/src/controller/Paymnet.controller.js
+++ Backend/src/controller/Paymnet.controller.js
@@ -1,19 +1,14 @@
-import Razorpay from "razorpay";
-
-export const PaymentInstance = new Razorpay({
-  key_id: process.env.Payment_Key_id,
-  key_secret: process.env.Payment_key_Secret,
-});
+import { PaymentInstance } from "../Libs/paymentGateway.js";
 
 export const TrialPayment = async (req, res) => {
   try {
     // const amount = req.body.amount*100
-    const amount = req.body.amount*100
-    console.log("amount is : ", amount)
-    console.log("amount is : ", amount)
+    const amount = req.body.amount * 100;
+    console.log("amount is : ", amount);
+    console.log("amount is : ", amount);
     const options = {
-    //   amount : Number(req.body.amount*100)  , // Razorpay expects amount in paise (so 100 INR = 10000)
-      amount : Number(amount)  , // Razorpay expects amount in paise (so 100 INR = 10000)
+      //   amount : Number(req.body.amount*100)  , // Razorpay expects amount in paise (so 100 INR = 10000)
+      amount: Number(amount), // Razorpay expects amount in paise (so 100 INR = 10000)
       currency: "INR",
     };
 
@@ -28,7 +23,34 @@ export const TrialPayment = async (req, res) => {
     console.log("Error in payment is : ", error);
     res.status(400).json({
       error: `Code fat gaya`,
-      error
+      error,
     });
+  }
+};
+
+export const createOrder = async (req, res) => {
+  try {
+    const { amount, currency = "INR", subscriptionPlaylistId } = req.body;
+    const { userId } = req.user.id;
+
+    const options = {
+      amount: amount * 100,
+      currency,
+      receipt: `receipt_${Date.now()}`,
+    };
+
+    const order = await PaymentInstance.orders.create(options);
+
+    res.status(200).json({
+      success: true,
+      orderId: order.id,
+      amount: order.amount,
+      Currency: order.currency,
+      subscriptionPlaylistId,
+      key: PaymentInstance.key,
+    });
+  } catch (error) {
+    console.error("Error creating Razorpay order:", error);
+    res.status(500).json({ success: false, error: "Order creation failed" });
   }
 };
